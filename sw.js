351fe70aa4ec853932791fe4738426913009f167
jamesmottrie <james.mottrie@edu.escp.eu>
2026-08-25 17:37:00 +0200

    Delete sw.js

--- sw.js
+++
@@ -1,8 +0,0 @@
-const CACHE = "traitement2-v1";
-const ASSETS = ["./","./index.html","./manifest.json","./icon-192.png","./icon-512.png","./apple-touch-icon.png"];
-self.addEventListener("install", e => { e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS))); self.skipWaiting(); });
-self.addEventListener("activate", e => {
-  e.waitUntil(caches.keys().then(k => Promise.all(k.filter(x => x !== CACHE).map(x => caches.delete(x)))));
-  self.clients.claim();
-});
-self.addEventListener("fetch", e => { e.respondWith(caches.match(e.request).then(r => r || fetch(e.request))); });
