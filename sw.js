6ed0c7ded5dbeaa4e041ce86357d2c25f9cb7a95
jamesmottrie <james.mottrie@edu.escp.eu>
2026-08-25 17:43:10 +0200

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,24 @@
+const CACHE = "cure-v1";
+const ASSETS = [
+  "./","./index.html","./manifest.json",
+  "./icon-192.png","./icon-512.png","./apple-touch-icon.png"
+];
+self.addEventListener("install", e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
+  self.skipWaiting();
+});
+self.addEventListener("activate", e => {
+  e.waitUntil(caches.keys().then(k => Promise.all(k.filter(x => x !== CACHE).map(x => caches.delete(x)))));
+  self.clients.claim();
+});
+// cache-first: l'app s'ouvre sans reseau
+self.addEventListener("fetch", e => {
+  if (e.request.method !== "GET") return;
+  e.respondWith(
+    caches.match(e.request).then(hit => hit || fetch(e.request).then(res => {
+      const copy = res.clone();
+      caches.open(CACHE).then(c => c.put(e.request, copy)).catch(()=>{});
+      return res;
+    }).catch(() => caches.match("./index.html")))
+  );
+});
